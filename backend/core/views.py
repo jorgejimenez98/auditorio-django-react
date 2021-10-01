31ddb6b37038f7be01f03bdcb36a8788e9f284ea
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-01 23:20:04 +0200

    CREATED user views and authentication

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import viewsets
+from backend import utils
+from .serializers import UserSerializerWithToken, UserSerializer
 
-# Create your views here.
+
+# AUTHENTICATION PROCESS
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        initialData = self.initial_data
+        try:
+            user = get_user_model().objects.get(email=initialData.get('email'))
+            if not user.check_password(initialData.get('password')):
+                return Response({'detail': utils.getLoginErrorMessage()}).data
+            data = super().validate(attrs)
+            serializer = UserSerializerWithToken(self.user).data
+            for k, v in serializer.items():
+                data[k] = v
+            return data
+        except get_user_model().DoesNotExist:
+            return Response({'detail': utils.getUserNotExistMessage(initialData.get('email'))}).data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# VIEW SET
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all().order_by('-pk')
+    permission_classes = [IsAdminUser, IsAuthenticated]
