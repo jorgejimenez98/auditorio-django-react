13d9beb59680d1e122f9823fc422e491f63587b1
Ulises31CmgU <ulises.abraham@zoho.com>
2021-09-30 23:50:56 -0400

    Add rootReducer, store and basic user setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+
+import store from "./redux/store";
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
   document.getElementById('root')
 );
 
---
+++ frontend/src/redux/user/user.reducer.js
@@ -0,0 +1,37 @@
+import {UserActionTypes} from "./user.types";
+import {combineReducers} from "redux";
+
+const INITIAL_STATE = {
+    token: localStorage.getItem('token'),
+}
+
+const userLoginReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case UserActionTypes.USER_LOGIN:
+            return {
+                ...state,
+            }
+        default:
+            return state;
+    }
+}
+
+const userLogoutReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case UserActionTypes.USER_LOGOUT:
+            return {
+                ...state,
+            }
+        default:
+            return state;
+    }
+}
+
+const userReducer = combineReducers(
+    {
+        userLogin: userLoginReducer,
+        userLogout: userLogoutReducer
+    }
+);
+
+export default userReducer;
