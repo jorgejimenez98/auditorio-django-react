b516d1f1073640545f4285ec7f8d6519c362f130
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-01 03:41:26 +0200

    UPDATE start of dashboard

--- frontend/src/components/Dashbard.js
+++ frontend/src/components/Dashbard.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../redux/user/user.actions";
 
-function Dashbard() {
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+function Dashbard({ history }) {
+  const dispatch = useDispatch();
+  // USER INFO Selector
+  const { userInfo } = useSelector((state) => state.user.userLogin);
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/login");
+    }
+  }, [history, userInfo]);
+  return (
+    <div>
+      Dashboard
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Log Out
+      </button>
+    </div>
+  );
 }
 
-export default Dashbard
+export default Dashbard;
