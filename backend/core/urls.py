b775860d991556ae0b3a8bc9d2e4fd27a7d2262d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-01 23:20:28 +0200

    ADDED user authentication urls to app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,4 +1,13 @@
+# django-rest-framework imports
+from rest_framework import routers
+# Django imports
 from django.urls import path
+# Login View and View Set
+from .views import MyTokenObtainPairView, UserViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
+    path('users/login/', MyTokenObtainPairView.as_view(), name='loginView'),
 ]
