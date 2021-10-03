2a9e5cd07e54bd426c79198c6344f34ab9172fe9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-04 01:16:47 +0200

    UPDATE routes, remove some routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,58 +1,36 @@
+// Icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
 // core components/views for Admin layout
 import DashboardPage from "./views/Dashboard/Dashboard.js";
 import UserProfile from "./views/UserProfile/UserProfile.js";
-import TableList from "./views/TableList/TableList.js";
-import Typography from "./views/Typography/Typography.js";
-import Maps from "./views/Maps/Maps.js";
-import NotificationsPage from "./views/Notifications/Notifications.js";
+import UserList from "./views/Users/UserList.js";
 
 const dashboardRoutes = [
+  // SideBar Routes
   {
     path: "/dashboard",
     name: "Dashboard",
     icon: Dashboard,
     component: DashboardPage,
     layout: "/admin",
+    sidebar: true,
   },
   {
-    path: "/drivers",
-    name: "Drivers",
-    icon: "content_paste",
-    component: TableList,
-    layout: "/admin",
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: LibraryBooks,
-    component: Typography,
-    layout: "/admin",
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: Notifications,
-    component: NotificationsPage,
-    layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: LocationOn,
-    component: Maps,
-    layout: "/admin",
-  },
-  {
-    path: "/user",
-    name: "User Profile",
+    path: "/user-list",
+    name: "Listado de Usuarios",
+    component: UserList,
     icon: Person,
+    layout: "/admin",
+    sidebar: true,
+  },
+  // Other Routes
+  {
+    path: "/user-profile",
+    name: "Perfíl de Usuario",
     component: UserProfile,
     layout: "/admin",
+    sidebar: false,
   },
 ];
 
