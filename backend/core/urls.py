9a61ea99a59f6f687891f4b50fe409f7113324f2
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-05 01:40:09 +0200

    ADD changePersonalData url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,11 +3,12 @@ from rest_framework import routers
 # Django imports
 from django.urls import path
 # Login View and View Set
-from .views import MyTokenObtainPairView, UserViewSet
+from .views import MyTokenObtainPairView, UserViewSet, changePersonalData
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
 
 urlpatterns = [
-    path('users/login/', MyTokenObtainPairView.as_view(), name='loginView')
+    path('users/login/', MyTokenObtainPairView.as_view(), name='loginView'),
+    path('users/changePersonalData/', changePersonalData, name='changePersonalData'),
 ]
