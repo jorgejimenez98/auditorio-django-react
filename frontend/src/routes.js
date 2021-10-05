3bee746ff5f6ef381fb978ea87665431499814a2
Ulises31CmgU <ulises.abraham@zoho.com>
2021-10-04 22:27:18 -0400

    FEAT add all sidebar links

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,24 @@
 // Icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
+import {
+  AiFillFilePpt, 
+  FaFileSignature,
+  GiFullFolder,
+  HiDocumentReport,
+  BsGraphDown,
+  FaFileInvoice,
+} from 'react-icons/all';
 // core components/views for Admin layout
-import DashboardPage from "./views/Dashboard/Dashboard.js";
-import UserProfile from "./views/UserProfile/UserProfile.js";
-import UserList from "./views/Users/UserList.js";
+import DashboardPage from "./views/Dashboard/Dashboard";
+import UserProfile from "./views/UserProfile/UserProfile";
+import UserList from "./views/Users/UserList";
+import DeficiencySummaryComponent from "./views/deficiency-summary/deficiency-summary.component";
+import FileComponent from './views/file/file.component';
+import FinalReportComponent from './views/final-report/final-report.component';
+import WorkOrderComponent from './views/work-order/work-order.component';
+import WorkSheetComponent from './views/work-sheet/work-sheet.component';
+import YearPlanComponent from './views/year-plan/year-plan.component';
 
 const dashboardRoutes = [
   // SideBar Routes
@@ -13,6 +27,54 @@ const dashboardRoutes = [
     name: "Dashboard",
     icon: Dashboard,
     component: DashboardPage,
+    layout: "/admin",
+    sidebar: true,
+  },
+  {
+    path: "/year-plan",
+    name: "Plan anual",
+    component: YearPlanComponent,
+    icon: AiFillFilePpt,
+    layout: "/admin",
+    sidebar: true,
+  },
+  {
+    path: "/work-order",
+    name: "Orden de trabajo",
+    component: WorkOrderComponent,
+    icon: FaFileSignature,
+    layout: "/admin",
+    sidebar: true,
+  },
+  {
+    path: "/file",
+    name: "Expediente",
+    component: FileComponent,
+    icon: GiFullFolder,
+    layout: "/admin",
+    sidebar: true,
+  },
+  {
+    path: "/final-report",
+    name: "Reporte",
+    component: FinalReportComponent,
+    icon: HiDocumentReport,
+    layout: "/admin",
+    sidebar: true,
+  },
+  {
+    path: "/deficiency-summary",
+    name: "Resumen de deficiencia",
+    component: DeficiencySummaryComponent,
+    icon: BsGraphDown,
+    layout: "/admin",
+    sidebar: true,
+  },
+  {
+    path: "/work-sheet",
+    name: "Plantilla de trabajo",
+    component: WorkSheetComponent,
+    icon: FaFileInvoice,
     layout: "/admin",
     sidebar: true,
   },
