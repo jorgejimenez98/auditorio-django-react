4b8debbe557b260894ef29cd6b8a23f47c28e1e2
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-05 03:17:50 +0200

    CREATE index to export the util containers

---
+++ frontend/src/containers/index.js
@@ -0,0 +1,6 @@
+import Loader from "./Loader";
+import Message from "./Message";
+import SnackBar from "./SnackBar";
+import AddButtomListHeader from "./AddButtomListHeader";
+
+export { Loader, Message, SnackBar, AddButtomListHeader };
