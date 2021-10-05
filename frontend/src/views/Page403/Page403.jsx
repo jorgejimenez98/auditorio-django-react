ef2fc2c29b2f0d39cbce968a34bba47cbf281b98
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-05 02:20:55 +0200

    UPDATE 403 component

--- frontend/src/views/Page403/Page403.jsx
+++ frontend/src/views/Page403/Page403.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@material-ui/core";
+import { LinkContainer } from "react-router-bootstrap";
 
-function Page403() {
-    return (
-        <div>
-            Page 403
+const Page403 = () => {
+  return (
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-5">
+            <div className="clearfix">
+              <h1 className="float-left display-3 mr-4">403</h1>
+              <h4 className="pt-3">Error. Acceso denegado.</h4>
+              <p className="text-muted float-left">
+                Usted no tiene permisos para acceder a esta Url del sistema
+              </p>
+            </div>
+            <div className="text-center">
+              <LinkContainer to="/">
+                <Button color="info">Volver al Sistema</Button>
+              </LinkContainer>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Page403
-
+export default Page403;
