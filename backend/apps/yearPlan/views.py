f3caeb30144a7f3ab60286351134a6460cc0127e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-31 00:16:49 +0200

    ADD retrieve method to year plan view set

--- backend/apps/yearPlan/views.py
+++ backend/apps/yearPlan/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from .serializers import YearPlanSerializer, YearPlan
-from rest_framework import viewsets
+from .serializers import YearPlanSerializer, YearPlan, YearPlanMiniSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 
 class YearPlanViewSet(viewsets.ModelViewSet):
     queryset = YearPlan.objects.all().order_by('-year')
-    serializer_class = YearPlanSerializer
+    serializer_class = YearPlanMiniSerializer
     permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = YearPlanSerializer(instance, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
