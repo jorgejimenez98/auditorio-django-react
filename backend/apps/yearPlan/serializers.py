1448e346d59761fb217ca39aa4df0d76d9f5de66
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 23:19:45 +0200

    ADD get work orders to year plan serializer

--- backend/apps/yearPlan/serializers.py
+++ backend/apps/yearPlan/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import YearPlan
+from .apps.workOders.serializers import WorkOrderMiniSerializer
 
 
 class YearPlanSerializer(serializers.ModelSerializer):
+    workOrders = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = YearPlan
         fields = '__all__'
+        extra_kwargs = {'workOrders': {'required': True}}
+
+    def get_workOrders(self, obj):
+        serializer = WorkOrderMiniSerializer(obj.workOrders.all(), many=True)
+        return serializer.data
