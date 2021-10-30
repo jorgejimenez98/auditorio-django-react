dec2b808bfe9b74cff3bb0dcc7d0c6e77ef7da6d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 23:19:11 +0200

    CREATE workOrder view sets

--- backend/apps/workOrder/views.py
+++ backend/apps/workOrder/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import WorkOrderMiniSerializer, WorkOrderSerializer, WorkOrder
 
-# Create your views here.
+
+class WorkOrderViewSet(viewsets.ModelViewSet):
+    queryset = WorkOrder.objects.all().order_by('-pk')
+    serializer_class = WorkOrderMiniSerializer
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = WorkOrderSerializer(instance, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
