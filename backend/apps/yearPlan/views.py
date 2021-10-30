358894b3f9c5ae8454a3f65143b140594abeb23c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-30 22:20:04 +0200

    CREATE year plan viewset

--- backend/apps/yearPlan/views.py
+++ backend/apps/yearPlan/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import YearPlanSerializer, YearPlan
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class YearPlanViewSet(viewsets.ModelViewSet):
+    queryset = YearPlan.objects.all().order_by('-year')
+    serializer_class = YearPlanSerializer
+    permission_classes = [IsAuthenticated]
