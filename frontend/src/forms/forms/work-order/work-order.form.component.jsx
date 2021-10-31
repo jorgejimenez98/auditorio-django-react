16c887f6b14ddcb6d74852ddc7f06c3cdd6fca6d
Ulises31CmgU <ulises.abraham@zoho.com>
2021-10-30 20:50:01 -0400

    FIX work order form

--- frontend/src/forms/forms/work-order/work-order.form.component.jsx
+++ frontend/src/forms/forms/work-order/work-order.form.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 import * as Yup from 'yup';
-
+import { useSelector } from "react-redux";
 import { Form, Formik } from 'formik';
 import Button from "react-bootstrap/Button";
 
+import { yupOptions } from '../../common/yup-option';
+
 import CustomTextInputComponent from "../../custom-components/custom-text-input.component";
 
-function WorkOrderFormComponent({values=null, otherHandleSubmit=false, ...otherProps}) {
-    
+function WorkOrderFormComponent() {
+    const today = new Date();
+
+
     const handleSubmit = (values) => {
         alert(JSON.stringify(values))
     }
@@ -17,86 +21,67 @@ function WorkOrderFormComponent({values=null, otherHandleSubmit=false, ...otherP
         <div>
             <Formik
             initialValues={{
-                entidad: 0,
-                codNIT: '',
-                codREEUP: '',
-                tipoAccion: '',
-                directiva: '',
-                particularidades: '',
-                cantAudit: '',
-                diasHabiles: '',
-                diasAudit: '',
+                // meta values
+                date: today.toLocaleDateString(),
+                time: today.toLocaleTimeString(),
+                author: useSelector((state) => state.user.userLogin).userInfo.name,
+                // step 1
+                yearPlan: {}, // plan anual al que pertenece(objeto, opciones)
+                noWO: 0, // numero de orden de trabajo(numero, opciones)
+                // step 2
+                directives: {}, // directivas y objetivos(textos)
+                criteria: '', // criterios (texto)
+                system: '', // sistema automatizado a aplicar (texto)
+                staff: {        // auditores designados (texto)
+                    leader: '',
+                    auditor1: '',
+                    auditor2: '',
+                },
+                // step 3
+                entity: '', // entidad a auditar (texto)
+                subordinated: '', // subordinada a ... (texto)
+                address: '', // direccion (texto)
+                province: '', // provincia (texto, opciones)
+                municipality: '', // municipio (texto, opciones)
+                NAE: '', // nomenclatura de la actividad economica (texto)
+                financingForm: '', // forma de financiamiento (texto, opciones)
+                FORG: '', // forma organizativa (texto)
+                cubanStateEntrpSys: '', // Sistema empresarial estatal cubano (texto, opciones)
+                isPerfecting: false, // empresa en perfeccionamiento (boolean)
+                merchantSociety: false, // sociedad mercantil (boolean)
             }}
-            validationSchema={Yup.object(
-
-            )}
+            validationSchema={Yup.object({
+                // yearPlan: yupOptions.firstName_r,
+                // noWO: yupOptions.firstName_r,
+                // directives: yupOptions.firstName_r,
+                // criteria: yupOptions.firstName_r,
+                // system: yupOptions.firstName_r,
+                // staff: yupOptions.firstName_r,
+                // entity: yupOptions.firstName_r,
+                // subordinated: yupOptions.firstName_r,
+                // address: yupOptions.firstName_r,
+                // province: yupOptions.firstName_r,
+                // municipality: yupOptions.firstName_r,
+                // NAE: yupOptions.firstName_r,
+                // financingForm: yupOptions.firstName_r,
+                // FORG: yupOptions.firstName_r,
+                // cubanStateEntrpSys: yupOptions.firstName_r,
+            })}
             onSubmit={(values, {setSubmitting})=>{
-                if(otherHandleSubmit){
-                    otherHandleSubmit(values)
-                }else{
-                    handleSubmit(values)
-                }
-                setSubmitting(true)
+                handleSubmit(values)
+                setSubmitting(false)
             }}
             >
                 <Form>
-                <CustomTextInputComponent
+                    <CustomTextInputComponent
                         label="Entidad"
                         name="entidad"
-                        type="number"
-                        placeholder="Jane"
-                    /> 
-                <CustomTextInputComponent
-                        label="Codigo NIT"
-                        name="codNIT"
                         type="text"
                         placeholder="Jane"
                     /> 
-                <CustomTextInputComponent
-                        label="Codigo REEUP"
-                        name="codREEUP"
-                        type="text"
-                        placeholder="Jane"
-                    /> 
-                <CustomTextInputComponent
-                        label="Accion"
-                        name="tipoAccion"
-                        type="text"
-                        placeholder="Jane"
-                    /> 
-                <CustomTextInputComponent
-                        label="Directiva"
-                        name="directiva"
-                        type="text"
-                        placeholder="Jane"
-                    /> 
-                <CustomTextInputComponent
-                        label="Particularidades"
-                        name="particularidades"
-                        type="text"
-                        placeholder="Jane"
-                    /> 
-                <CustomTextInputComponent
-                        label="Cantidad de Auditores"
-                        name="cantAudit"
-                        type="text"
-                        placeholder="Jane"
-                    /> 
-                <CustomTextInputComponent
-                        label="Dias habiles"
-                        name="diasHabiles"
-                        type="text"
-                        placeholder="Jane"
-                    /> 
-                <CustomTextInputComponent
-                        label="Dias de auditoria"
-                        name="diasAudit"
-                        type="text"
-                        placeholder="Jane"
-                    /> 
-                    {!otherHandleSubmit &&
-                        <Button type='submit'>Enviar</Button>
-                    }
+                  
+                    <Button type='submit'>Enviar</Button>
+                    
                 </Form>
             </Formik>
         </div>
