7534c7fc66c940ca84877d3e6b490ab6c3ec3184
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 00:44:32 +0100

    Remove retrieve method from workorders view set

--- backend/apps/workOrder/views.py
+++ backend/apps/workOrder/views.py
@@ -6,10 +6,5 @@ from .serializers import WorkOrderMiniSerializer, WorkOrderSerializer, WorkOrder
 
 class WorkOrderViewSet(viewsets.ModelViewSet):
     queryset = WorkOrder.objects.all().order_by('-pk')
-    serializer_class = WorkOrderMiniSerializer
+    serializer_class = WorkOrderSerializer
     permission_classes = [IsAuthenticated]
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = WorkOrderSerializer(instance, many=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
