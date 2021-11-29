2f23ac25e9f7834a5c0c582aac6bf6f134147974
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 00:44:04 +0100

    Update work order model

--- backend/apps/workOrder/models.py
+++ backend/apps/workOrder/models.py
@@ -4,36 +4,42 @@ from ..yearPlan.models import YearPlan
 
 
 class WorkOrder(models.Model):
-    datetime = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
-    yearPlan = models.ForeignKey(
-        YearPlan, on_delete=models.PROTECT, related_name='workOrders')
+    dateTimeCreated = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=255, default="")
+    yearPlan = models.ForeignKey(YearPlan, on_delete=models.PROTECT, related_name='workOrders')
     noWO = models.PositiveSmallIntegerField(default=0)
-    criteria = models.CharField(max_length=256)
-    system = models.CharField(max_length=256)
-    leader = models.CharField(max_length=256)
-    auditor1 = models.CharField(max_length=256)
-    auditor2 = models.CharField(max_length=256)
-    entity = models.CharField(max_length=256)
-    subordinated = models.CharField(max_length=256)
-    address = models.CharField(max_length=256)
-    province = models.CharField(max_length=256)
-    municipality = models.CharField(max_length=256)
-    NAE = models.CharField(max_length=256)
-    financingForm = models.CharField(max_length=256)
-    FORG = models.CharField(max_length=256)
-    cubanStateEntrpSys = models.CharField(max_length=256)
+    criteria = models.TextField(default="")
+    system = models.CharField(max_length=256, default="")
+    leader = models.CharField(max_length=256, default="")
+    auditor1 = models.CharField(max_length=256, default="")
+    auditor2 = models.CharField(max_length=256, default="")
+    entity = models.CharField(max_length=256, default="")
+    subordinated = models.CharField(max_length=256, default="")
+    address = models.CharField(max_length=256, default="")
+    province = models.CharField(max_length=256, default="")
+    municipality = models.CharField(max_length=256, default="")
+    NAE = models.CharField(max_length=256, default="")
+    cubanStateEntrpSys = models.CharField(max_length=256, default="")
     isPerfecting = models.BooleanField(default=False)
     merchantSociety = models.BooleanField(default=False)
+    codNIT = models.CharField(max_length=11, default="")
+    codREEUP = models.CharField(max_length=8, default="")
+    actionType = models.CharField(max_length=255, default="")
+    unidadPress = models.BooleanField(default=False)
+    cantAuditores = models.PositiveSmallIntegerField(default=0)
+    diasHabiles = models.PositiveIntegerField(default=30)
+    startDate = models.DateField(default=None, blank=True, null=True)
+    endDate = models.DateField(default=None, blank=True, null=True)
 
     def __str__(self):
         return f"Work Order {self.id}"
+    
+
 
 
 class Directive(models.Model):
     text = models.CharField(max_length=128, default="")
-    workOrders = models.ForeignKey(
-        WorkOrder, on_delete=models.PROTECT, related_name='directives')
+    workOrders = models.ForeignKey(WorkOrder, on_delete=models.PROTECT, related_name='directives')
 
     def __str__(self):
         return self.text
