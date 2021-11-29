c11ef5b2df9face66b4e8d42cff552379b1a2b6b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-29 23:54:47 +0100

    Create Inventory and Inventory Items model

--- backend/apps/inventory/models.py
+++ backend/apps/inventory/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from apps.yearPlan.models import YearPlan
+from apps.workOrder.models import WorkOrder
 
-# Create your models here.
+
+class Inventory(models.Model):
+    author = models.CharField(max_length=255)
+    yearPlan = models.ForeignKey(YearPlan, on_delete=models.PROTECT, related_name='inventories')
+    workOrder = models.OneToOneField(WorkOrder, on_delete=models.PROTECT, related_name='inventory')
+    dateTimeCreate = models.DateTimeField(auto_now_add=True)
+
+class InventoryItem(models.Model):
+    inventory = models.ForeignKey(Inventory, on_delete=models.PROTECT, related_name='inventoryItems')
+    sContCant = models.FloatField(default=0)
+    sContTotal = models.FloatField(default=0)
+    sSubCant = models.FloatField(default=0)
+    sSubPrice = models.FloatField(default=0)
+    sTeCant = models.FloatField(default=0)
+    sTeTotal = models.FloatField(default=0)
+    um = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    diff = models.FloatField(default=0)
