ac89ea54cda8d574e5959cb83b3fce64050a1d59
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 00:45:05 +0100

    Remove retrieve method of year plan view set

--- backend/apps/yearPlan/views.py
+++ backend/apps/yearPlan/views.py
@@ -3,14 +3,10 @@ from .serializers import YearPlanSerializer, YearPlan, YearPlanMiniSerializer
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from apps.extraPermissions import IsAuditor
 
 
 class YearPlanViewSet(viewsets.ModelViewSet):
     queryset = YearPlan.objects.all().order_by('-year')
-    serializer_class = YearPlanMiniSerializer
-    permission_classes = [IsAuthenticated]
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = YearPlanSerializer(instance, many=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    serializer_class = YearPlanSerializer
+    permission_classes = [IsAuthenticated, IsAuditor]
