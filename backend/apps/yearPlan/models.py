7776843b8003dad5ec30568fd4c725cf5ddb30ea
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 03:11:39 +0100

    Add author attr to year plan model

--- backend/apps/yearPlan/models.py
+++ backend/apps/yearPlan/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class YearPlan(models.Model):
     year = models.PositiveIntegerField(default=0, unique=True)
+    author = models.CharField(max_length=255, default="")
     cantidadAudit = models.PositiveSmallIntegerField(default=0)
     diasAudit = models.PositiveSmallIntegerField(default=0)
     diasFeriad = models.PositiveSmallIntegerField(default=0)
@@ -10,6 +11,7 @@ class YearPlan(models.Model):
     diasCapacitacion = models.PositiveSmallIntegerField(default=0)
     diasReservas = models.PositiveSmallIntegerField(default=0)
     controlInterno = models.PositiveSmallIntegerField(default=0)
+
 
     def __str__(self):
         return f"Year {self.year} {self.cantidadAudit}"
