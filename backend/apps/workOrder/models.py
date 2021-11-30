be037287f51af21f757ac496518a1546b5ea0cce
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 03:10:41 +0100

    Update model work order, add two method

--- backend/apps/workOrder/models.py
+++ backend/apps/workOrder/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from ..yearPlan.models import YearPlan
-
+from datetime import datetime, timedelta
+import maya
 
 class WorkOrder(models.Model):
     dateTimeCreated = models.DateTimeField(auto_now_add=True)
@@ -10,6 +11,7 @@ class WorkOrder(models.Model):
     noWO = models.PositiveSmallIntegerField(default=0)
     criteria = models.TextField(default="")
     system = models.CharField(max_length=256, default="")
+    auditType = models.CharField(max_length=256, default="")
     leader = models.CharField(max_length=256, default="")
     auditor1 = models.CharField(max_length=256, default="")
     auditor2 = models.CharField(max_length=256, default="")
@@ -19,9 +21,12 @@ class WorkOrder(models.Model):
     province = models.CharField(max_length=256, default="")
     municipality = models.CharField(max_length=256, default="")
     NAE = models.CharField(max_length=256, default="")
+    FORG = models.BooleanField(default=False)
     cubanStateEntrpSys = models.CharField(max_length=256, default="")
     isPerfecting = models.BooleanField(default=False)
     merchantSociety = models.BooleanField(default=False)
+
+    """ Estos se sacan desde el year plan """
     codNIT = models.CharField(max_length=11, default="")
     codREEUP = models.CharField(max_length=8, default="")
     actionType = models.CharField(max_length=255, default="")
@@ -34,7 +39,17 @@ class WorkOrder(models.Model):
     def __str__(self):
         return f"Work Order {self.id}"
     
-
+    def updateDates(self, datetime_str):
+        dateTimeObject = maya.parse(datetime_str[:10]).datetime()
+        self.startDate = dateTimeObject
+        self.endDate = self.startDate + timedelta(self.diasHabiles)
+        self.save()
+    
+    def updateAuditors(self, auditors):
+        self.leader = auditors[0]
+        self.auditor1 = auditors[1] if len(auditors) >= 2 else ""
+        self.auditor2 = auditors[2] if len(auditors) >= 3 else ""
+        self.save()
 
 
 class Directive(models.Model):
