c5b37c5ea550f998dd590ac62130373682882ad8
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-01 15:40:49 +0100

    Update work orders table columns

--- frontend/src/mui-datatables/work-order/work-order.table.setting.js
+++ frontend/src/mui-datatables/work-order/work-order.table.setting.js
@@ -1,230 +1,256 @@
+import { LinkContainer } from "react-router-bootstrap";
+import { Tooltip, IconButton } from "@material-ui/core";
+import { Edit } from "@material-ui/icons";
+
 const columns = [
-    // yearPlan entity subordinated codNIT codREEUP
-    // startDate endDate 
-    {
-        name: "author",
-        label: "Autor",
-        options: {
-            hint: '',
-        }
+  // yearPlan entity subordinated codNIT codREEUP
+  // startDate endDate
+  {
+    name: "id",
+    label: "",
+    options: {
+      filter: false,
+      sort: false,
+      display: false,
+      viewColumns: false,
     },
-    {
-        name: "yearPlan",
-        label: "Plan Anual",
-        options: {
-            hint: '',
-            filterType: 'dropdown',
-        }
+  },
+  {
+    name: "author",
+    label: "Autor",
+    options: {
+      hint: "",
     },
-    {
-        name: "noWO",
-        label: "Numero de orden",
-        options: {
-            hint: '',
-        }
+  },
+  {
+    name: "yearPlan",
+    label: "Plan Anual",
+    options: {
+      hint: "",
+      filterType: "dropdown",
     },
-    {
-        name: "auditType",
-        label: "Tipo de auditoria",
-        options: {
-            hint: '',
-        }
+  },
+  {
+    name: "noWO",
+    label: "Numero de orden",
+    options: {
+      hint: "",
     },
-    {
-        name: "actionType",
-        label: "Tipo de accion",
-        options: {
-            hint: '',
-        }
+  },
+  {
+    name: "auditType",
+    label: "Tipo de auditoria",
+    options: {
+      hint: "",
     },
-    {
-        name: "criteria",
-        label: "Criterio",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "actionType",
+    label: "Tipo de accion",
+    options: {
+      hint: "",
     },
-    {
-        name: "directives",
-        label: "Directivas",
-        options: {
-            hint: '',
-            filter: false,
-            display: false,
-            viewColumns: false
-        }
+  },
+  {
+    name: "criteria",
+    label: "Criterio",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "system",
-        label: "Sistema automatizado a aplicar",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "directives",
+    label: "Directivas",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
+      viewColumns: false,
     },
-    {
-        name: "staff",
-        label: "Equipo de trabajo",
-        options: {
-            hint: '',
-            filter: false,
-            display: false,
-            viewColumns: false
-        }
+  },
+  {
+    name: "system",
+    label: "Sistema automatizado a aplicar",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "entity",
-        label: "Entidad a auditar",
-        options: {
-            hint: '',
-        }
+  },
+  {
+    name: "staff",
+    label: "Equipo de trabajo",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
+      viewColumns: false,
     },
-    {
-        name: "subordinated",
-        label: "Subordinada a ...",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "entity",
+    label: "Entidad a auditar",
+    options: {
+      hint: "",
     },
-    {
-        name: "address",
-        label: "Direccion",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "subordinated",
+    label: "Subordinada a ...",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "province",
-        label: "Provincia",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "address",
+    label: "Direccion",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "municipality",
-        label: "Municipio",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "province",
+    label: "Provincia",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "codNIT",
-        label: "Codigo NIT",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "municipality",
+    label: "Municipio",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "codREEUP",
-        label: "Codigo REUUP",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "codNIT",
+    label: "Codigo NIT",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "NAE",
-        label: "Nomenclatura de la actividad economica",
-        options: {
-            hint: '',
-            display: false
-        }
+  },
+  {
+    name: "codREEUP",
+    label: "Codigo REUUP",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "FORG",
-        label: "Forma organizaiva",
-        options: {
-            hint: '',
-            filter: false,
-            display: false,
-            viewColumns: false
-        }
+  },
+  {
+    name: "NAE",
+    label: "Nomenclatura de la actividad economica",
+    options: {
+      hint: "",
+      display: false,
     },
-    {
-        name: "unidadPres",
-        label: "Unidad presupuestada",
-        options: {
-            hint: '',
-            filter: false,
-            display: false,
-            viewColumns: false
-        }
+  },
+  {
+    name: "FORG",
+    label: "Forma organizaiva",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
+      viewColumns: false,
     },
-    {
-        name: "cubanStateEntrpSys",
-        label: "Sistema empresarial Cubano",
-        options: {
-            hint: '',
-            filter: false,
-            display: false
-        }
+  },
+  {
+    name: "unidadPres",
+    label: "Unidad presupuestada",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
+      viewColumns: false,
     },
-    {
-        name: "isPerfecting",
-        label: "Empresa en perfeccionamiento",
-        options: {
-            hint: '',
-            filter: false,
-            display: false,
-            viewColumns: false
-        }
+  },
+  {
+    name: "cubanStateEntrpSys",
+    label: "Sistema empresarial Cubano",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
     },
-    {
-        name: "merchantSociety",
-        label: "Sociedad mercantil",
-        options: {
-            hint: '',
-            filter: false,
-            display: false,
-            viewColumns: false
-        }
+  },
+  {
+    name: "isPerfecting",
+    label: "Empresa en perfeccionamiento",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
+      viewColumns: false,
     },
-    {
-        name: "startDate",
-        label: "Fecha de inicio",
-        options: {
-            hint: '',
-        }
+  },
+  {
+    name: "merchantSociety",
+    label: "Sociedad mercantil",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
+      viewColumns: false,
     },
-    {
-        name: "endDate",
-        label: "Fecha de fin",
-        options: {
-            hint: '',
-            filter: false,
-            display: false,
-            viewColumns: false
-        }
+  },
+  {
+    name: "startDate",
+    label: "Fecha de inicio",
+    options: {
+      hint: "",
     },
-    {
-        name: "actions",
-        label: "Acciones",
-        options: {
-            hint: '',
-            filter: false,
-            sort: false,
-            download: false,
-            print: false,
-            searchable: false,
-            viewColumns: false,
-        }
+  },
+  {
+    name: "endDate",
+    label: "Fecha de fin",
+    options: {
+      hint: "",
+      filter: false,
+      display: false,
+      viewColumns: false,
     },
+  },
+  {
+    name: "actions",
+    label: "Acciones",
+    options: {
+      hint: "",
+      filter: false,
+      sort: false,
+      download: false,
+      print: false,
+      searchable: false,
+      viewColumns: false,
+      customBodyRender: (value, tableMeta) => {
+        const id = tableMeta.rowData[0];
+        return (
+          <LinkContainer to={`/admin/word-order/edit/${id}`}>
+            <Tooltip title="Editar Orden de Trabajo">
+              <IconButton>
+                <Edit />
+              </IconButton>
+            </Tooltip>
+          </LinkContainer>
+        );
+      },
+    },
+  },
 ];
 
 const options = {
-    filterType: 'textField',
-    resizableColumns: true,
-    rowsPerPageOptions: [5, 10, 15],
-    selectToolbarPlacement: 'above',
-}
+  filterType: "textField",
+  resizableColumns: true,
+  rowsPerPageOptions: [5, 10, 15],
+  selectToolbarPlacement: "above",
+};
 
 export const workOrderTableSetting = {
-    columns: columns,
-    options: options,
-}
+  columns: columns,
+  options: options,
+};
