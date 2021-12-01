c3c61d98dd3462d008ce3dd26230d55f032c904b
Ulises31CmgU <ulises.abraham@zoho.com>
2021-12-01 13:25:34 -0500

    ADD redux config

---
+++ frontend/src/redux/year-plan/year-plan.actions.js
@@ -0,0 +1,27 @@
+import { YearPlanActionTypes } from "./year-plan.types";
+import axios from "axios";
+
+const listYearPlan = () => {
+
+}
+const createYearPlan = () => {
+
+}
+const updateYearPlan = () => {
+
+}
+const deleteYearPlan = () => {
+
+}
+
+const yearPlanActions = {
+    list: listYearPlan,
+    create: createYearPlan,
+    update: updateYearPlan,
+    delete: deleteYearPlan
+}
+
+export default yearPlanActions;
+
+
+
