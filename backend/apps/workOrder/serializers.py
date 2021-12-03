8720eeeb8dfd1f62dfe942dfa48a649e860559e2
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-03 23:10:48 +0100

    Add year plan id to work order serializer

--- backend/apps/workOrder/serializers.py
+++ backend/apps/workOrder/serializers.py
@@ -21,10 +21,11 @@ class WorkOrderMiniSerializer(serializers.ModelSerializer):
 class WorkOrderSerializer(serializers.ModelSerializer):
     directives = serializers.StringRelatedField(read_only=True, many=True)
     yearPlan = serializers.SerializerMethodField(read_only=True)
+    yearPlanId = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = WorkOrder
-        fields = ['id', 'dateTimeCreated', 'author', 'yearPlan', 'noWO', 'directives',
+        fields = ['id', 'dateTimeCreated', 'author', 'yearPlan', 'yearPlanId', 'noWO', 'directives',
                   'criteria', 'system', 'auditType', 'leader', 'auditor1', 'auditor2', 'entity',
                   'subordinated', 'address', 'province', 'municipality', 'NAE', 'FORG', 'cubanStateEntrpSys', 'isPerfecting',
                   'merchantSociety', 'codNIT', 'codREEUP', 'actionType', 'unidadPress', 'cantAuditores', 'diasHabiles', 'startDate', 'endDate']
@@ -35,6 +36,9 @@ class WorkOrderSerializer(serializers.ModelSerializer):
 
     def get_yearPlan(self, obj):
         return obj.yearPlan.year
+
+    def get_yearPlanId(self, obj):
+        return obj.yearPlan.id
 
 
 class WorkOrderDetailsSerializer(serializers.ModelSerializer):
